3c78263e375a42535ad08ebfc6256888e3831e69
spencer burke <info.spencer.burke@gmail.com>
2025-03-11 01:05:00 -0700

    Add the source files

---
+++ minflixfrontend/src/App.js
@@ -0,0 +1,12 @@
+import RegistrationForm from "./RegistrationForm";
+
+const App = () => {
+  return(
+    <div>
+      <h1>Hello from the form</h1>
+      <RegistrationForm/>
+    </div>
+  );
+};
+
+export default App;
---
+++ minflixfrontend/src/RegistrationForm.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react';
+
+const RegistrationForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPasswordChange = (event) => {
+        setShowPassword(event.target.checked);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            alert('Passwords do not match!');
+            return;
+        }
+
+        const formData = new FormData(event.target);
+
+        fetch('http://localhost:8000/registration', {
+            method: 'POST',
+            body: formData,
+            credentials: 'include'
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    };
+
+    return (
+        <form id='registrationForm' onSubmit={handleSubmit}>
+            <p>
+                <label htmlFor='email'>Email:</label>
+                <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                />
+            </p>
+            <p>
+                <label htmlFor='password'>Password:</label>
+                <input
+                    type={showPassword ? 'text' : 'password'}
+                    id='password'
+                    name='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                />
+            </p>
+            <p>
+                <label htmlFor='confirmPassword'>Confirm Password:</label>
+                <input
+                    type={showPassword ? 'text' : 'password'}
+                    id='confirmPassword'
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    required
+                />
+            </p>
+            <p>
+                <label htmlFor='showPassword'>Show Password </label>
+                <input
+                    id='showPassword'
+                    type='checkbox'
+                    checked={showPassword}
+                    onChange={handleShowPasswordChange}
+                />
+            </p>
+            <p>
+                <button type='submit'>Submit</button>
+            </p>
+        </form>
+    );
+};
+
+export default RegistrationForm;
