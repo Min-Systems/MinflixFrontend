265f188346a2102cb0bbd51fe3c055e6326d3381
antho-n-y <anthonyjimenez276@yahoo.com>
2025-04-06 11:01:03 -0700

    Save progress before initializing git flow

--- minflixfrontend/src/LoginPage.js
+++ minflixfrontend/src/LoginPage.js
@@ -1,16 +1,33 @@
 import React from "react";
+import './LoginPage.css';
 import { useNavigate } from 'react-router-dom';
 import AuthenticationForm from './AuthenticationForm'
+import 'boxicons'
+<link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
+
 
 const LoginPage = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
-            <h1>Login Page</h1>
+     <body>
+        <div class = "wrapper">
+            <h1>Login </h1>
+            
             <AuthenticationForm isLogin={true} />
-            <button onClick={() => navigate('/register')}>Go to Registration</button>
+
+
+           {/* <button onClick={() => navigate('/register')}>Go to Registration</button>  */}
+           <div class = "register-link">
+            <p> Dont have an account?
+            <a href="/register"> Register</a>
+            </p>
+           </div>
+    
+     
+    
         </div>
+     </body>
     );
 }
 
